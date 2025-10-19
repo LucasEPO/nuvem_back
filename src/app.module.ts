89eaa8412034d1dce428ed1480db8c125e3f6877
LucasEPO <lucas.epoliveira@gmail.com>
2025-10-19 17:07:30 -0300

    Add typeorm setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
+import { databaseProviders } from './database/database.providers';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    DatabaseModule
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ...databaseProviders],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = new DocumentBuilder()
+  .setTitle('Documentação da API - Nuvem')
+  .setDescription('Descrição da API com Swagger')
+  .setVersion('1.0')
+  .addBearerAuth()
+  .build();
+
+  app.enableCors({
+    origin: 'http://localhost:3000', //alterar ao subir aplicacao do site
+    methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'],
+    allowedHeaders: 'Content-Type, Authorization',
+  });
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
+  await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
