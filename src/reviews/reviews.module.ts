3242619eadc37bb96a53658b87c3c484b2ff6ba4
LucasEPO <lucas.epoliveira@gmail.com>
2025-10-23 13:41:27 -0300

    Add reviews module

--- src/reviews/reviews.module.ts
+++ src/reviews/reviews.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ReviewsService } from './reviews.service';
 import { ReviewsController } from './reviews.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Cart } from 'src/carts/entities/cart.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Cart])],
   controllers: [ReviewsController],
   providers: [ReviewsService],
+  exports: [ReviewsService],
 })
 export class ReviewsModule {}
